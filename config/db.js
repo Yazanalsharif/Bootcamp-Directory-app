75100a61a65df07f83902503719de0ccdb34e522
Yazanalsharif <63266097+Yazanalsharif@users.noreply.github.com>
2021-03-21 16:21:57 +0200

    add the database file

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+const connection = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+
+  console.log(
+    chalk.italic.underline.cyan(
+      `the database connect on: ${conn.connection.host}`
+    )
+  );
+};
+
+module.exports = connection;
